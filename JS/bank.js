8798d8aa805b270d7cd514dee86f7ce8e80f56f5
skpixels <makkobir79@gmail.com>
2023-03-13 22:56:52 +0600

    total balance oparation completed and new file added

--- JS/bank.js
+++ JS/bank.js
@@ -2,15 +2,21 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
   const newDepositAmount = parseFloat(depositField.value);
-  console.log(typeof newDepositAmount);
 
   // current deposit total
   const depositTotalAmount = document.getElementById("deposit-total");
   const previousDepositTotal = parseFloat(depositTotalAmount.innerText);
 
   const currentDepositTotal = previousDepositTotal + newDepositAmount;
-
+  // set total to deposit
   depositTotalAmount.innerText = currentDepositTotal;
+
+  // Current Balance total
+  const balanceTotalAmount = document.getElementById("balance-total");
+  const previousBalanceTotal = parseFloat(balanceTotalAmount.innerText);
+  //   set balance with new deposit
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  balanceTotalAmount.innerText = currentBalanceTotal;
 
   //   clear input field
   depositField.value = "";
