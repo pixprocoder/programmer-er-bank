90defe4b44bc6b5988f81568dc027fa267c00a1e
skpixels <makkobir79@gmail.com>
2023-03-15 12:31:36 +0600

    stpes file created and withdraw file commented

---
+++ JS/__steps.js
@@ -0,0 +1,14 @@
+/* 
+1. Add event handler with the withdraw button
+2. Get the withdraw amount from the withdraw input field
+2-5. Also parse it to a number by default it is string
+3. Get previous withdraw total
+
+4. Calculate total withdraw Amount
+4-5. Set total withdraw Amount
+5. Get the previous Balance total
+6. Calculate new Balance total
+6-5. Set the new Balance total
+7. Clear the input field
+
+*/
--- JS/withdraw.js
+++ JS/withdraw.js
@@ -1,17 +1,8 @@
 /* 
-1. Add event handler with the withdraw button
-2. Get the withdraw amount from the withdraw input field
-2-5. Also parse it to a number by default it is string
-3. Get previous withdraw total
+This code was created my be at the first time without function now I have learned the better way which I will implement above
+ */
 
-4. Calculate total withdraw Amount
-4-5. Set total withdraw Amount
-5. Get the previous Balance total
-6. Calculate new Balance total
-6-5. Set the new Balance total
-7. Clear the input field
-
-*/
+/* 
 // 1. Add event handler with the withdraw button
 document.getElementById("withdraw-btn").addEventListener("click", function () {
   // 2. Get the withdraw amount from the withdraw input field
@@ -42,4 +33,4 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
 
   // clear
   withdrawField.value = "";
-});
+}); */
